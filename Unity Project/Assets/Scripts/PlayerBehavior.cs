12fea14c581a9e7fa6750e9857a56e1c4b8d29a4
Gruppe-Neun <leon_buettinghaus@web.de>
2019-04-14 16:41:32 +0200

    Added walk and dab animations to player

--- Unity Project/Assets/Scripts/PlayerBehavior.cs
+++ Unity Project/Assets/Scripts/PlayerBehavior.cs
@@ -7,9 +7,15 @@ public class PlayerBehavior : MonoBehaviour
 
     public float moveSpeed = 10f;
 
-    public GameObject bullet;
+    public Vector3 verticalDirection;
 
+    public Vector3 horizontalDirection;
+
+    public GameObject bullet;
+    
     public CursorBehavior cursor;
+
+    public Animator animator;
 
 
     private bool shotLastFrame = false;
@@ -22,8 +28,11 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position += Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        horizontalDirection = Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.position += horizontalDirection;
+
+        verticalDirection = Vector3.up * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += verticalDirection;
 
         if(Input.GetMouseButtonDown(0)&&!shotLastFrame){
             GameObject bulletClone = Instantiate(bullet, transform.position, cursor.transform.rotation);
@@ -32,6 +41,41 @@ public class PlayerBehavior : MonoBehaviour
         }else{if(!Input.GetMouseButtonDown(0))
             shotLastFrame=false;
         }
+
+        // Animations
+
+        bool top = false;
+        bool bot = false;
+        bool left = false;
+        bool right = false;
+
+        float x = horizontalDirection.normalized.x;
+        float y = verticalDirection.normalized.y;
+
+        if(y == -1) {
+            bot = true;
+        }
         
+        if(y == 1) {
+            top = true;
+        }
+
+        if(y == 0 && x == -1) {
+            left = true;
+        }
+
+        if(y == 0 && x == 1) {
+            right = true;
+        }
+
+        animator.SetBool("top", top);
+        animator.SetBool("bot", bot);
+        animator.SetBool("left", left);
+        animator.SetBool("right", right);
+        animator.SetBool("dableft", Input.GetKey(KeyCode.Q));
+        animator.SetBool("dabright", Input.GetKey(KeyCode.E));
+
+        Debug.Log("Q: "+ Input.GetKey(KeyCode.Q));
+        Debug.Log("E: " + Input.GetKey(KeyCode.E));
     }
 }
